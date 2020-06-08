94c0c470b7ca5838d272e37b4d051f3884fdb57f
baronet2 <eth.baron@mail.utoronto.ca>
2020-06-08 13:26:51 -0400

    Work on enrollment R

--- R_Logit_Models/Location_Choice/CampusChoiceEnrol.R
+++ R_Logit_Models/Location_Choice/CampusChoiceEnrol.R
@@ -13,17 +13,17 @@ log_likelihoods = vector("numeric", num_segments)
 rho_squareds = vector("numeric", num_segments)
 for (i in 0:(num_segments-1))
 {
-  ml_data = mlogit.data(df, varying=3:30, choice="Campus", shape="wide",
+  ml_data = mlogit.data(df, varying=3:37, choice="Campus", shape="wide",
                         subset = Segment == i)
-
+  
   if (i == 0) model = mlogit(Campus ~ Dist + Total | 0, data=ml_data, reflevel="5")
   else if (i < 4) model = mlogit(Campus ~ Dist + UG | 0, data=ml_data, reflevel="5")
   else model = mlogit(Campus ~ Dist + Grad | 0, data=ml_data, reflevel="5")
   
-  model2 = mlogit(Campus ~ Dist, data=ml_data, reflevel="5")
+  model2 = mlogit(Campus ~ 0 | 1, data=ml_data, reflevel="5")
+  # print((model[[2]][[1]]) / (model2[[2]][[1]]) - 1)
   
   coefs_and_stats[num_intercepts+1, i+1] = model[[2]][[1]] # Log likelihood
-  print(1 - (model[[2]][[1]]) / (model2[[2]][[1]]))
   for (j in 1:num_intercepts)
   {
     coefs_and_stats[j, i+1] = model[[1]][[j]]
