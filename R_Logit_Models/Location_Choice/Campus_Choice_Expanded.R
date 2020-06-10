37e1433039ed676d07ab8903fb628c0d6e447d5e
baronet2 <eth.baron@mail.utoronto.ca>
2020-06-10 17:19:14 -0400

    Expansion output

--- R_Logit_Models/Location_Choice/Campus_Choice_Expanded.R
+++ R_Logit_Models/Location_Choice/Campus_Choice_Expanded.R
@@ -1,39 +1,55 @@
+# Import packages
 library(data.table)
 library(mlogit)
 
+# Load data
 setwd("C:/Users/ethan/Documents/Ethan/TMG/Research/PORPOS-TMG/R_Logit_Models/Location_Choice")
-
 df <- read.csv("Segment_Exp_Input.csv")
+
+# Reformat data
 df$Campus = as.factor(df$Campus)
 
-
-
+# Prepare output matrix
 num_segments = 7L
-# coefs_and_stats = array(numeric(), c(9, num_segments))
+num_intercepts = 7L
+coefs_and_stats = array(numeric(), c(num_intercepts + 2, num_segments*4))
 
+# Iterate over segments
 for (i in 0:(num_segments-1))
 {
+  # Prepare data in long format
   ml_data = mlogit.data(df, varying=4:31, choice="Campus", shape="wide", subset=Segment==i)
   
+  # Run models
   ref = mlogit(Campus ~ Dist, data=ml_data, reflevel="YG")
   smt = mlogit(Campus ~ Dist, data=ml_data, reflevel="YG", weights=ml_data$Exp_SMTO)
   tot = mlogit(Campus ~ Dist, data=ml_data, reflevel="YG", weights=ml_data$Exp_Total)
   lev = mlogit(Campus ~ Dist, data=ml_data, reflevel="YG", weights=ml_data$Exp_Level)
 
-  # coefs_and_stats[1, i+1] = ref[[2]][[1]] # Log likelihood
-  # coefs_and_stats[2, i+1] = enr[[2]][[1]] # Log likelihood
-  # coefs_and_stats[3, i+1] = dst[[2]][[1]] # Log likelihood
-  # coefs_and_stats[4, i+1] = end[[2]][[1]] # Log likelihood
-  # coefs_and_stats[5, i+1] = grv[[2]][[1]] # Log likelihood
-  # 
-  # coefs_and_stats[6, i+1] = 1 - (enr[[2]][[1]])/(ref[[2]][[1]]) # Rho squared
-  # coefs_and_stats[7, i+1] = 1 - (dst[[2]][[1]])/(ref[[2]][[1]]) # Rho squared
-  # coefs_and_stats[8, i+1] = 1 - (end[[2]][[1]])/(ref[[2]][[1]]) # Rho squared
-  # coefs_and_stats[9, i+1] = 1 - (grv[[2]][[1]])/(ref[[2]][[1]]) # Rho squared
+  # Write estimated parameters
+  for (j in 1:num_intercepts)
+  {
+    coefs_and_stats[j, (4*i)+1] = ref[[1]][[j]]
+    coefs_and_stats[j, (4*i)+2] = smt[[1]][[j]]
+    coefs_and_stats[j, (4*i)+3] = tot[[1]][[j]]
+    coefs_and_stats[j, (4*i)+4] = lev[[1]][[j]]
+  }
+  
+  # Write log likelihoods
+  coefs_and_stats[j+1, (4*i)+1] = ref[[2]][[1]]
+  coefs_and_stats[j+1, (4*i)+2] = smt[[2]][[1]]
+  coefs_and_stats[j+1, (4*i)+3] = tot[[2]][[1]]
+  coefs_and_stats[j+1, (4*i)+4] = lev[[2]][[1]]
+  
+  # Write rho-squared
+  coefs_and_stats[j+2, (4*i)+1] = summary(ref)[[20]][[1]]
+  coefs_and_stats[j+2, (4*i)+2] = summary(smt)[[20]][[1]]
+  coefs_and_stats[j+2, (4*i)+3] = summary(tot)[[20]][[1]]
+  coefs_and_stats[j+2, (4*i)+4] = summary(lev)[[20]][[1]]
 }
 
-# d = as.data.frame(coefs_and_stats)
-# setnames(d, old = c('V1','V2', 'V3', 'V4', 'V5', 'V6', 'V7'), new = c("0", "1", "2", "3", "4", "5", "6"))
-# write.csv(d, "Comparison_Output.csv", row.names = c("REF_LLH", "ENR_LLH", "DST_LLH", "E+D_LLH", "GRV_LLH", "ENR_R^2", "DST_R^2", "E+D_R^2", "GRV_R^2"))
-
+# Print output to file
+d = as.data.frame(coefs_and_stats)
+setnames(d, old = c('V1','V2', 'V3', 'V4'), new = c("Ref.", "SMTO", "By Total", "By Level"))
+write.csv(d, "Exp_Comparison.csv", row.names = c("ASC_SG", "ASC_SC", "ASC_MI", "ASC_YK", "ASC_RY", "ASC_OC", "B_DIST", "L_LHOOD", "MCF_R^2"))
 
