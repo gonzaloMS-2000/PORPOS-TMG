9dd05149991164df5cad625ea63bdef2db9652af
baronet2 <eth.baron@mail.utoronto.ca>
2020-06-18 16:48:58 -0400

    Logsums

--- R_Logit_Models/Combined/Proposed Model.R
+++ R_Logit_Models/Combined/Proposed Model.R
@@ -38,18 +38,16 @@ campus_df$Segment = as.factor(campus_df$Segment)
 campus_df$Licence = as.factor(campus_df$Licence)
 campus_df$Family = as.factor(campus_df$Family)
 
-# Expansion factors
-campus_df$Exp = ifelse(campus_df$Segment == 1, campus_df$Exp_SMTO, campus_df$Exp_Level)
-
 for (i in 0:6)
 {
   print(i)
-  temp_df = campus_df[, c(2, 4, 5, (i*4)+7, (i*4)+8, (i*4)+10)]
+  temp_df = campus_df[, c(2:4, (i*4)+6, (i*4)+7, (i*4)+9)]
   names(temp_df)[names(temp_df)==paste0("Time.Auto.", i)] <- "Time.Auto"
   names(temp_df)[names(temp_df)==paste0("Time.Transit.", i)] <- "Time.Transit"
   names(temp_df)[names(temp_df)==paste0("Time.Active.", i)] <- "Time.Active"
   temp_df$Mode = "Transit"
   temp_mldf = mlogit.data(temp_df, varying = 4:6, choice = "Mode", shape = "wide")
+  mldf_av = mldf[mldf$available]
   access = as.data.frame(logsum(model, data=temp_mldf))
   names(access) <- paste0("Access.", i)
   campus_df = cbind(campus_df, access)
@@ -58,6 +56,34 @@ for (i in 0:6)
 
 # ---- Location choice ----
 
-# df2 = mldf2 to df format
-# loc_mldf = mlogit.data(df2, varying = Campuses)
-# model = ... (weights)
+# Convert data to long format
+campus_df = campus_df[c(1, 5, seq(8, 32, by=4), 34:41)]
+lc_mldf = mlogit.data(campus_df, varying = c(3:9, 11:17), choice = "Campus", shape = "wide")
+
+# Iterate over segments
+for (i in 0:6)
+{
+  print(i)
+  # Run model
+  # ref = mlogit(Campus ~ Dist, data=lc_mldf, reflevel="4",
+  #              subset = lc_mldf$Segment == i)
+  # wei = mlogit(Campus ~ Dist, data=lc_mldf, reflevel="4",
+  #              subset = lc_mldf$Segment == i, weights=lc_mldf$Exp)
+  # print(paste(i, summary(ref)[[20]][1], summary(wei)[[20]][1]))
+  
+  lc_model = mlogit(Campus ~ 1, data=lc_mldf, reflevel="4",
+                    weights=lc_mldf$Exp, subset = lc_mldf$Segment == i)
+  print(lc_model[[2]][1])
+
+  lc_model_dist = mlogit(Campus ~ Dist, data=lc_mldf, reflevel="4",
+                    weights=lc_mldf$Exp, subset = lc_mldf$Segment == i)
+  print(lc_model_dist[[2]][1])
+
+  lc_model_access = mlogit(Campus ~ Access, data=lc_mldf, reflevel="4",
+                    weights=lc_mldf$Exp, subset = lc_mldf$Segment == i)
+  print(lc_model_access[[2]][1])
+
+  lc_model_both = mlogit(Campus ~ Dist + Access, data=lc_mldf, reflevel="4",
+                    weights=lc_mldf$Exp, subset = lc_mldf$Segment == i)
+  print(lc_model_both[[2]][1])
+}
