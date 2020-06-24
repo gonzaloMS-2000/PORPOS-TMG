5037f43355567a38211fb40c4ed19cd6e8a141df
baronet2 <eth.baron@mail.utoronto.ca>
2020-06-24 17:26:06 -0400

    Metrics output

--- R_Logit_Models/Location_Choice/Metrics/SimpleLCMetrics.R
+++ R_Logit_Models/Location_Choice/Metrics/SimpleLCMetrics.R
@@ -1,34 +1,61 @@
+# ---- Packages ----
 library(data.table)
 library(mlogit)
 library(caret)
 library(e1071)
 library(mltools)
 
-setwd("C:/Users/ethan/Documents/Ethan/TMG/Research/PORPOS-TMG/R_Logit_Models/Location_Choice/Simple")
-
+# ---- Data ----
+setwd("C:/Users/ethan/Documents/Ethan/TMG/Research/PORPOS-TMG/R_Logit_Models/Location_Choice/Metrics")
 df <- read.csv("../Campus_Choice_Segments.csv")
 df$Campus = as.factor(df$Campus)
-
 ml_data = mlogit.data(df, varying=2:8, choice="Campus", shape="wide")
 
+# ---- Prepare Output ----
+num_segments = 7L
+num_campuses = 7L
+num_trials = 5L
+num_metrics = 6L # Accuracy, Macro PRF, MCC, APO
+metrics = array(numeric(), c(num_trials, num_metrics)) 
+output = array(numeric(), c(num_segments + 1, num_campuses + num_metrics))
+
+# ---- Reference Model ----
 model = mlogit(Campus ~ Dist, data=ml_data, reflevel="0")
-summary(model)
-
-preds = vector()
+for (j in 1:num_campuses) output[1, j] = model[[1]][[j]]
 x = fitted(model, outcome = FALSE)
-for (i in 1:nrow(x))
+for (j in 1:num_trials)
 {
-  preds = c(preds, sample(7, 1, prob = x[i,]) - 1)
+  preds = vector()
+  for (i in 1:nrow(x)) preds = c(preds, sample(7, 1, prob = x[i,]) - 1)
+  preds = as.factor(preds)
+  y = confusionMatrix(preds, df$Campus)
+  metrics[j,] = c(y[[3]][1], mean(y[[4]][,5]), mean(y[[4]][,6]), mean(y[[4]][,7]),
+                  mcc(preds=preds, actuals=df$Campus), mean(fitted(model)))
 }
-preds = as.factor(preds)
+output[1, (num_campuses+1):(num_campuses+num_metrics)] = apply(metrics, 2, mean)
+
+# ---- Segmented Model ----
+for (i in 1:num_segments)
+{
+  model = mlogit(Campus~Dist, data=ml_data, reflevel="0", subset=ml_data$Segment==(i-1))
+  for (j in 1:num_campuses) output[i+1, j] = model[[1]][[j]]
   
-y = confusionMatrix(preds, df$Campus)
-cm = y[[2]] # Access elements with y[i, j]
-acc = y[[3]] # Accuracy, Kappa, Lower, Upper, Null, P-Val, McNemar
+  x = fitted(model, outcome = FALSE)
+  for (j in 1:num_trials)
+  {
+    preds = vector()
+    for (k in 1:nrow(x)) preds = c(preds, sample(7, 1, prob = x[k,]) - 1)
+    preds = as.factor(preds) # TODO manually set levels to correspond to df$Campus
+    y = confusionMatrix(preds, subset(df, Segment == i-1)$Campus)
+    y[[4]][,5][is.na(y[[4]][,5])] = 0
+    y[[4]][,7][is.na(y[[4]][,7])] = 0
+    
+    metrics[j,] = c(y[[3]][1], mcc(preds=preds, actuals=subset(df, Segment == i-1)$Campus), mean(fitted(model)),
+                    mean(y[[4]][,5]), mean(y[[4]][,6]), mean(y[[4]][,7]))
+   
+}
+  output[i+1, (num_campuses+1):(num_campuses+num_metrics)] = apply(metrics, 2, mean)
+}
 
-stats = y[[4]]
-macro_prf = c(mean(stats[,5]), mean(stats[,6]), mean(stats[,7])) # Note: minor difference between f1 and 2PR / (P+R)
-mcc = mcc(preds = preds, actuals = df$Campus) # Matthews
-
-cat("Micro Accuracy\t", acc[1], "\nMacro Precision\t", macro_prf[1], "\nMacro Recall\t", macro_prf[2],
-    "\nMacro F-1 Score\t", macro_prf[3], "\nMatthews Coeff\t", mcc, "\nAve Prob of Obs\t", mean(fitted(model)))
+# ---- Send Output ----
+write.csv(output, "MetricsOutput.csv")
