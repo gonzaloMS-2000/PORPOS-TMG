e7f28f755f02558a0d1650a1367f974bf75e3129
baronet2 <eth.baron@mail.utoronto.ca>
2020-06-25 15:42:48 -0400

    Update code for new data

--- R_Logit_Models/Location_Choice/Campus Variables/CampusVars.R
+++ R_Logit_Models/Location_Choice/Campus Variables/CampusVars.R
@@ -5,20 +5,21 @@ library(caret)
 library(e1071)
 library(mltools)
 
-# ---- Data ----
+# ---- Load and Format Data ----
 setwd("C:/Users/ethan/Documents/Ethan/TMG/Research/PORPOS-TMG/R_Logit_Models/Location_Choice/Campus Variables")
 df <- read.csv("../../../Data/SMTO_2015/SMTO_2015_Complete_Input.csv")
 
 factor_cols = c(1:7, 13:16)
 df[factor_cols] = lapply(df[factor_cols], factor)
 df$Family = ifelse(df$Family == "Family", 1, 0)
-df$Income = ifelse(df$Income == "High", 2, ifelse(df$Income == "Low", 1, 1.266)) # Average for unknown
+# df$Income = ifelse(df$Income == "High", 2, ifelse(df$Income == "Low", 1, 1.266)) # Average for unknown
 
 mldf = mlogit.data(df, varying=17:86, choice="School_Codes", shape="wide")
 mldf$Domestic = mldf$Domestic * 100
 mldf$Tuition = mldf$Tuition / 100
-mldf$Admission_Avg = ifelse(mldf$Admission_Avg > 1, mldf$Admission_Avg, mldf$Admission_Avg * 100)
+mldf$Admission_Avg = mldf$Admission_Avg * 100
 
+# ---- Run Models ----
 model = mlogit(School_Codes ~ Dist, data=mldf, reflevel="YG", weights = mldf$Exp) # 0.270
 model = mlogit(School_Codes ~ AIVTT, data=mldf, reflevel="YG", weights = mldf$Exp) # 0.264
 model = mlogit(School_Codes ~ TPTT, data=mldf, reflevel="YG", weights = mldf$Exp) # 0.256
