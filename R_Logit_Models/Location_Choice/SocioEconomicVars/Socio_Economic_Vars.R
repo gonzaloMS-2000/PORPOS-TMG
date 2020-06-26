2cfcd1f1b3362350ea622cf9f4791eb7797ef28a
gonzaloMS-2000 <65040979+gonzaloMS-2000@users.noreply.github.com>
2020-06-26 17:35:06 -0400

    more runs

--- R_Logit_Models/Location_Choice/SocioEconomicVars/Socio_Economic_Vars.R
+++ R_Logit_Models/Location_Choice/SocioEconomicVars/Socio_Economic_Vars.R
@@ -21,7 +21,7 @@ metrics = array(numeric(), c(num_trials, num_metrics))
 output = array(numeric(), c(num_segments + 1, num_campuses + num_metrics + 1))
 
 # ---- Reference Model ----
-model = mlogit(School_Codes ~ Dist | Level, data=ml_data, reflevel="YG")
+model = mlogit(School_Codes ~ Dist | Family + Level + Cars + Mode + Licence + Income, data=ml_data, reflevel="YG")
 summary = summary(model)
 summary(model)
 print(cat('test: ', model[[1]][[1]]))
@@ -41,31 +41,31 @@ output[1, (num_campuses+1):(num_campuses+num_metrics)] = apply(metrics, 2, mean)
 output[1,14] = summary[[20]][[1]]
 
 # ---- Segmented Model ----
-for (i in 1:num_segments)
-{
-  model = mlogit(School_Codes~ Dist | Level, data=ml_data, reflevel="YG", subset=ml_data$Segment==(i-1))
-  summary = summary(model)
-  print(summary(model))
-  for (j in 1:num_campuses) output[i+1, j] = model[[1]][[j]]
-
-  x = fitted(model, outcome = FALSE)
-  for (j in 1:num_trials)
-  {
-    preds = vector()
-    for (k in 1:nrow(x)) preds = c(preds, sample(7, 1, prob = x[k,]) - 1)
-    preds = as.factor(preds) # TODO manually set levels to correspond to df$Campus
-    levels(preds) = levels(df$School_Codes)
-    y = confusionMatrix(preds, subset(df, Segment == i-1)$School_Codes)
-    y[[4]][,5][is.na(y[[4]][,5])] = 0
-    y[[4]][,7][is.na(y[[4]][,7])] = 0
-
-    metrics[j,] = c(y[[3]][1], mean(y[[4]][,5]), mean(y[[4]][,6]), mean(y[[4]][,7]),
-                    mcc(preds=preds, actuals=subset(df, Segment == i-1)$School_Codes),
-                    mean(fitted(model)))
-  }
-  output[i+1, (num_campuses+1):(num_campuses+num_metrics)] = apply(metrics, 2, mean)
-  output[i+1,14] = summary[[20]][[1]]
-}
+# for (i in 1:num_segments)
+# {
+#   model = mlogit(School_Codes~ Dist | Level, data=ml_data, reflevel="YG", subset=ml_data$Segment==(i-1))
+#   summary = summary(model)
+#   print(summary(model))
+#   for (j in 1:num_campuses) output[i+1, j] = model[[1]][[j]]
+# 
+#   x = fitted(model, outcome = FALSE)
+#   for (j in 1:num_trials)
+#   {
+#     preds = vector()
+#     for (k in 1:nrow(x)) preds = c(preds, sample(7, 1, prob = x[k,]) - 1)
+#     preds = as.factor(preds) # TODO manually set levels to correspond to df$Campus
+#     levels(preds) = levels(df$School_Codes)
+#     y = confusionMatrix(preds, subset(df, Segment == i-1)$School_Codes)
+#     y[[4]][,5][is.na(y[[4]][,5])] = 0
+#     y[[4]][,7][is.na(y[[4]][,7])] = 0
+# 
+#     metrics[j,] = c(y[[3]][1], mean(y[[4]][,5]), mean(y[[4]][,6]), mean(y[[4]][,7]),
+#                     mcc(preds=preds, actuals=subset(df, Segment == i-1)$School_Codes),
+#                     mean(fitted(model)))
+#   }
+#   output[i+1, (num_campuses+1):(num_campuses+num_metrics)] = apply(metrics, 2, mean)
+#   output[i+1,14] = summary[[20]][[1]]
+# }
 
 # ---- Print Output ----
 print(output)
