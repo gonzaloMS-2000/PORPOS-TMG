48a1ae0ba203ea0123244a988aa233dfaf1f367e
gonzaloMS-2000 <65040979+gonzaloMS-2000@users.noreply.github.com>
2020-07-03 11:29:34 -0400

    took out mode runs

--- R_Logit_Models/Location_Choice/SocioEconomicVars/Socio_Economic_Vars.R
+++ R_Logit_Models/Location_Choice/SocioEconomicVars/Socio_Economic_Vars.R
@@ -9,37 +9,35 @@ library(mltools)
 setwd("C:/Users/gonza/Desktop/University/Summer 2020/TMG/GitHub_PORPOS/PORPOS-TMG/R_Logit_Models/Location_Choice/SocioEconomicVars")
 
 # Formulas
-# formulas = list(c(mFormula(School_Codes ~ Dist | 1), 0),
-#                 c(mFormula(School_Codes ~ Dist | Licence), 1),
-#                 c(mFormula(School_Codes ~ Dist | Cars), 1),
-#                 c(mFormula(School_Codes ~ Dist | Mode), 1),
-#                 c(mFormula(School_Codes ~ Dist | Licence + Cars), 2),
-#                 c(mFormula(School_Codes ~ Dist | Licence + Mode), 2),
-#                 c(mFormula(School_Codes ~ Dist | Cars + Mode), 2),
-#                 c(mFormula(School_Codes ~ Dist | Licence + Cars + Mode), 3),
-#                 c(mFormula(School_Codes ~ Dist | Income), 1),
-#                 c(mFormula(School_Codes ~ Dist | Income + Mode), 2),
-#                 c(mFormula(School_Codes ~ Dist | Income + Cars), 2),
-#                 c(mFormula(School_Codes ~ Dist | Income + Cars + Mode), 3),
-#                 c(mFormula(School_Codes ~ Dist | Income + Cars + Licence + Mode), 4),
-#                 c(mFormula(School_Codes ~ Dist | Income + Children), 2),
-#                 c(mFormula(School_Codes ~ Dist | Income + Children + Adults), 3),
-#                 c(mFormula(School_Codes ~ Dist | Income + Cars + Licence + Mode + Children + Adults), 6))
-
-formulas = list(c(mFormula(School_Codes ~ Dist | Cars + Family + Level), 1))
-
-formulas = list(c(mFormula(School_Codes ~ Dist | Family), 1),
-                c(mFormula(School_Codes ~ Dist | Level), 1),
-                c(mFormula(School_Codes ~ Dist | Status), 1),
-                c(mFormula(School_Codes ~ Dist | Family + Level), 2),
-                c(mFormula(School_Codes ~ Dist | Family + Status), 2),
-                c(mFormula(School_Codes ~ Dist | Income), 1),
+formulas = list(c(mFormula(School_Codes ~ Dist | 1), 0),
+                c(mFormula(School_Codes ~ Dist | Licence), 1),
                 c(mFormula(School_Codes ~ Dist | Cars), 1),
                 c(mFormula(School_Codes ~ Dist | Licence + Cars), 2),
-                c(mFormula(School_Codes ~ Dist | Licence + Cars + Income), 3),
-                c(mFormula(School_Codes ~ Dist | Family + Level + Cars), 3),
-                c(mFormula(School_Codes ~ Dist | Family + Level + Cars + Licence), 4),
-                c(mFormula(School_Codes ~ Dist | Family + Level + Cars + Licence + Income), 5))
+                c(mFormula(School_Codes ~ Dist | Income), 1),
+                c(mFormula(School_Codes ~ Dist | Income + Cars), 2),
+                c(mFormula(School_Codes ~ Dist | Income + Cars + Licence), 3),
+                c(mFormula(School_Codes ~ Dist | Income + Children), 2),
+                c(mFormula(School_Codes ~ Dist | Income + Adults), 2),
+                c(mFormula(School_Codes ~ Dist | Income + Cars + Children), 3),
+                c(mFormula(School_Codes ~ Dist | Income + Cars + Adults), 3),
+                c(mFormula(School_Codes ~ Dist | Income + Cars + Licence + Children), 4),
+                c(mFormula(School_Codes ~ Dist | Income + Cars + Licence + Adults), 4),
+                c(mFormula(School_Codes ~ Dist | Income + Children + Adults), 3),
+                c(mFormula(School_Codes ~ Dist | Income + Cars + Children + Adults), 4),
+                c(mFormula(School_Codes ~ Dist | Income + Cars + Licence + Children + Adults), 5))
+
+# formulas = list(c(mFormula(School_Codes ~ Dist | Family), 1),
+#                 c(mFormula(School_Codes ~ Dist | Level), 1),
+#                 c(mFormula(School_Codes ~ Dist | Status), 1),
+#                 c(mFormula(School_Codes ~ Dist | Family + Level), 2),
+#                 c(mFormula(School_Codes ~ Dist | Family + Status), 2),
+#                 c(mFormula(School_Codes ~ Dist | Income), 1),
+#                 c(mFormula(School_Codes ~ Dist | Cars), 1),
+#                 c(mFormula(School_Codes ~ Dist | Licence + Cars), 2),
+#                 c(mFormula(School_Codes ~ Dist | Licence + Cars + Income), 3),
+#                 c(mFormula(School_Codes ~ Dist | Family + Level + Cars), 3),
+#                 c(mFormula(School_Codes ~ Dist | Family + Level + Cars + Licence), 4),
+#                 c(mFormula(School_Codes ~ Dist | Family + Level + Cars + Licence + Income), 5))
 
 # ---- Prepare Output ----
 num_segments = 7L
@@ -81,37 +79,37 @@ for (call_idx in 1:length(formulas)) {
   output[current_row, (num_campuses+1):(num_campuses+num_metrics)] = apply(metrics, 2, mean)
   output[current_row,14] = summary[[20]][[1]]
   
-  # # ---- Segmented Model ----
-  # for (i in 1:num_segments)
-  # {
-  #   
-  #   model = mlogit(formulas[[call_idx]][[1]], data=ml_data, reflevel="YG", subset=ml_data$Segment==(i-1))
-  #   summary = summary(model)
-  #   print(summary(model))
-  #   #for (j in 1:num_campuses) output[current_row + i+1, j] = model[[1]][[j]]
-  #   for (j in 1:num_campuses) output[current_row + i, j] = model[[1]][[j]]
-  # 
-  #   x = fitted(model, outcome = FALSE)
-  #   for (j in 1:num_trials)
-  #   {
-  #     preds = vector()
-  #     for (k in 1:nrow(x)) preds = c(preds, colnames(x)[sample(7, 1, prob = x[k,])])
-  #     #for (i in 1:nrow(x)) preds = c(preds, colnames(x)[sample(7, 1, prob = x[i,])])
-  #     preds = as.factor(preds) 
-  #     levels(preds) = levels(df$School_Codes)
-  #     y = confusionMatrix(preds, subset(df, Segment == i-1)$School_Codes)
-  #     y[[4]][,5][is.na(y[[4]][,5])] = 0
-  #     y[[4]][,7][is.na(y[[4]][,7])] = 0
-  # 
-  #     metrics[j,] = c(y[[3]][1], mean(y[[4]][,5]), mean(y[[4]][,6]), mean(y[[4]][,7]),
-  #                     mcc(preds=preds, actuals=subset(df, Segment == i-1)$School_Codes),
-  #                     mean(fitted(model)))
-  #   }
-  #   output[current_row + i, (num_campuses+1):(num_campuses+num_metrics)] = apply(metrics, 2, mean)
-  #   output[current_row + i, 14] = summary[[20]][[1]]
-  # }
+  # ---- Segmented Model ----
+  for (i in 1:num_segments)
+  {
 
-  current_row = current_row + 2
+    model = mlogit(formulas[[call_idx]][[1]], data=ml_data, reflevel="YG", subset=ml_data$Segment==(i-1))
+    summary = summary(model)
+    print(summary(model))
+    #for (j in 1:num_campuses) output[current_row + i+1, j] = model[[1]][[j]]
+    for (j in 1:num_campuses) output[current_row + i, j] = model[[1]][[j]]
+
+    x = fitted(model, outcome = FALSE)
+    for (j in 1:num_trials)
+    {
+      preds = vector()
+      for (k in 1:nrow(x)) preds = c(preds, colnames(x)[sample(7, 1, prob = x[k,])])
+      #for (i in 1:nrow(x)) preds = c(preds, colnames(x)[sample(7, 1, prob = x[i,])])
+      preds = as.factor(preds)
+      levels(preds) = levels(df$School_Codes)
+      y = confusionMatrix(preds, subset(df, Segment == i-1)$School_Codes)
+      y[[4]][,5][is.na(y[[4]][,5])] = 0
+      y[[4]][,7][is.na(y[[4]][,7])] = 0
+
+      metrics[j,] = c(y[[3]][1], mean(y[[4]][,5]), mean(y[[4]][,6]), mean(y[[4]][,7]),
+                      mcc(preds=preds, actuals=subset(df, Segment == i-1)$School_Codes),
+                      mean(fitted(model)))
+    }
+    output[current_row + i, (num_campuses+1):(num_campuses+num_metrics)] = apply(metrics, 2, mean)
+    output[current_row + i, 14] = summary[[20]][[1]]
+  }
+
+  current_row = current_row + 11
   
 }  
   
