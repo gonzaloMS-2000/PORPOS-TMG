2e29527a76daff029e8c6be1385040c3b026eec2
baronet2 <eth.baron@mail.utoronto.ca>
2020-07-07 13:52:20 -0400

    Segmented

--- R_Logit_Models/Location_Choice/SocioEconomicVars/Socio_Economic_Vars_Next_Steps.R
+++ R_Logit_Models/Location_Choice/SocioEconomicVars/Socio_Economic_Vars_Next_Steps.R
@@ -15,14 +15,11 @@ df$Segment = as.factor(df$Segment)
 ml_data = mlogit.data(df, varying=18:87, choice="School_Codes", shape="wide")
 
 # ---- Formulas ----
-# formulas = c(mFormula(School_Codes ~ Dist | 1),
-#              mFormula(School_Codes ~ Dist | Work),
-#              mFormula(School_Codes ~ Dist | Work + Cars),
-#              mFormula(School_Codes ~ Dist | Cars + Adults + Children + Income + Licence + Work))
-
-# formulas = c(mFormula(School_Codes ~ Dist | Cars + Adults + Children + Licence + Work))
-formulas = c(mFormula(School_Codes ~ Dist | Cars + Adults + Income + Licence + Work))
-
+formulas = c(mFormula(School_Codes ~ Dist | 1),
+             mFormula(School_Codes ~ Dist | Work),
+             mFormula(School_Codes ~ Dist | Work + Cars),
+             mFormula(School_Codes ~ Dist | Cars + Adults + Children + Income + Licence + Work))
+alt_formula = mFormula(School_Codes ~ Dist | Cars + Adults + Income + Licence + Work)
 
 # ---- Prepare Output ----
 num_segments = 6L
@@ -34,24 +31,22 @@ output = array(numeric(), c((num_segments) * length(formulas), num_campuses + nu
 
 # --- Run Models ----
 for (i in 1:length(formulas)) {
-  print(i)
-  for (j in 6:num_segments) {
+  for (j in 1:num_segments) {
+    cat(i, ", ", j)
     actuals = subset(df, Segment==j)$School_Codes
-    cat(j, levels(actuals), "\n")
-    model = mlogit(formulas[[i]], data=ml_data, reflevel="SG", subset = ml_data$Segment==j, weights=ml_data$Exp_Segment)
+    if (i==length(formulas) & j==6) model = mlogit(alt_formula, data=ml_data, reflevel="SG", subset = ml_data$Segment==j, weights=ml_data$Exp_Segment)
+    else model = mlogit(formulas[[i]], data=ml_data, reflevel="SG", subset = ml_data$Segment==j, weights=ml_data$Exp_Segment)
     x = fitted(model, outcome = FALSE)
     for (k in 1:num_campuses) output[(i-1)*num_segments + j, k] = model[[1]][[j]]
     for (k in 1:num_trials) {
       preds = vector()
       for (l in 1:nrow(x)) preds = c(preds, colnames(x)[sample(7, 1, prob = x[l,])])
       preds = as.factor(preds)
-      print(levels(preds))
       y = confusionMatrix(preds, actuals)
       metrics[k,] = c(y[[3]][1], mean(y[[4]][,5]), mean(y[[4]][,6]), mean(y[[4]][,7]), mcc(preds=preds, actuals=actuals))
     }
     output[(i-1)*num_segments + j, (num_campuses+1):(num_campuses+num_metrics)] = apply(metrics, 2, mean)
     output[(i-1)*num_segments + j, (num_campuses+num_metrics+1):(num_campuses+num_metrics+2)] = c(mean(fitted(model)), summary(model)[[20]][[1]])
-    print(mean(fitted(model)))
   }
 }  
 
