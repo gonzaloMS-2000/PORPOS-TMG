c8df5ac8b68dee13d8c1d0128b06bd5cabf2c32a
baronet2 <eth.baron@mail.utoronto.ca>
2020-07-09 12:28:07 -0400

    New proposed

--- R_Logit_Models/Location_Choice/Proposed/Proposed.R
+++ R_Logit_Models/Location_Choice/Proposed/Proposed.R
@@ -9,14 +9,22 @@ mldf = mlogit.data(df, choice="School_Codes", shape="wide", varying = 18:87)
 mldf$Domestic = mldf$Domestic * 100
 mldf$Enrol = ifelse(mldf$Level == "UG", mldf$UG, ifelse(mldf$Level == "Grad", mldf$Grad, mldf$Total))
 
-# ---- Run Model ----
+# ---- Previous Model ----
 model = mlogit(School_Codes ~ Dist + Family:Dist + Level:Dist + Status:Dist + Family:Domestic | Level,
                data=mldf, weights=mldf$Exp_Segment, reflevel="SG", subset=mldf$Segment!=0)
 print(summary(model))
 print(mean(fitted(model)))
 
-# ---- Previous Model ----
+# ---- Updated Model ----
 model = mlogit(School_Codes ~ Dist + Family:Dist + Level:Dist + Status:Dist + Family:Domestic + Enrol | 0,
                data=mldf, weights=mldf$Exp_Segment, reflevel="SG", subset=mldf$Segment!=0)
 print(summary(model))
 print(mean(fitted(model)))
+
+# ---- Simpler Model ----
+for (i in 1:1) {
+  model = mlogit(School_Codes ~ Dist + Family:Dist + Enrol | 0,
+                 data=mldf, weights=mldf$Exp_Segment, reflevel="SG", subset=mldf$Segment!=0)
+  print(summary(model))
+  cat("APO:", mean(fitted(model)))
+}
