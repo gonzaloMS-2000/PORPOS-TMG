2789bc1004259e22bed8d53c39eb7d8692857e87
baronet2 <eth.baron@mail.utoronto.ca>
2020-07-16 11:51:04 -0400

    Clean metrics codE

--- R_Logit_Models/Location_Choice/Proposed/HardmaxProposed.R
+++ R_Logit_Models/Location_Choice/Proposed/HardmaxProposed.R
@@ -12,23 +12,25 @@ df$School_Codes = as.factor(df$School_Codes)
 mldf = mlogit.data(df, choice="School_Codes", shape="wide", varying = 18:87)
 mldf$Enrol = ifelse(mldf$Level == "UG", mldf$UG, ifelse(mldf$Level == "Grad", mldf$Grad, mldf$Total))
 
-# ---- Simpler Model ----
-num_trials = 1L
+# ---- Variables ----
+num_campuses = 7L
 num_metrics = 5L # Accuracy, Macro PRF, MCC
-metrics = array(numeric(), c(num_trials, num_metrics)) 
-for (i in 1:1) {
-  actuals = subset(df, Segment!=0)$School_Codes
-  model = mlogit(School_Codes ~ Dist + Family:Dist + Enrol | 0,
-                 data=mldf, weights=mldf$Exp_Segment, reflevel="SG", subset=mldf$Segment!=0)
-  probs = fitted(model, outcome = FALSE)
-  for (j in 1:num_trials) {
-    preds = vector()
-    for (i in 1:nrow(probs)) preds = c(preds, colnames(probs)[which.max(probs[i,])[[1]]])
-    preds = as.factor(preds)
-    y = confusionMatrix(preds, actuals)
-    metrics[j,] = c(y[[3]][1], mean(y[[4]][,5]), mean(y[[4]][,6]), mean(y[[4]][,7]), mcc(preds=preds, actuals=actuals))
-  }
-  print(metrics)
-  print(summary(model))
-  cat("APO:", mean(fitted(model)))
-}
+actuals = subset(df, Segment!=0)$School_Codes
+preds = vector()
+
+# ---- Run Model ----
+model = mlogit(School_Codes ~ Dist + Family:Dist + Enrol | 0, data=mldf, weights=mldf$Exp_Segment, reflevel="SG", subset=mldf$Segment!=0)
+probs = fitted(model, outcome = FALSE)
+
+# ---- Calculate Metrics ----
+for (i in 1:nrow(probs)) preds = c(preds, colnames(probs)[which.max(probs[i,])[[1]]])
+preds = as.factor(preds)
+levels(preds) = c(levels(preds), setdiff(levels(actuals), levels(preds)))
+y =  suppressWarnings(confusionMatrix(preds, actuals))
+prec = sum(y[[4]][,5], na.rm=TRUE) / num_campuses
+f1 = sum(y[[4]][,7], na.rm=TRUE) / num_campuses
+metrics = c(y[[3]][1], prec, mean(y[[4]][,6]), f1, mcc(preds=preds, actuals=actuals), mean(fitted(model)))
+names(metrics) = c("Acc", "Prec", "Rec", "F1", "MCC", "APO")
+
+# ---- Print Results ----
+metrics
