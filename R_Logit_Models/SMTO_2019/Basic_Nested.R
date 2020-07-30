3b78195f3fb5475dd7a3870611a6c1328a8abd89
baronet2 <eth.baron@mail.utoronto.ca>
2020-07-30 14:41:44 -0400

    Nested models lrtest

--- R_Logit_Models/SMTO_2019/Basic_Nested.R
+++ R_Logit_Models/SMTO_2019/Basic_Nested.R
@@ -15,24 +15,32 @@ df$Work = as.factor(df$Work)
 df$Licence = as.logical.factor(df$Liv_Arr)
 unis = levels(droplevels(subset(df, School_Type == 'University')$School))
 cols = setdiff(levels(df$School), unis)
-mldf = mlogit.data(df, choice="School", shape="wide", varying = 19:45)
+mldf = mlogit.data(df, choice="School", shape="wide", varying = 17:70)
 
 # Reference (Unnested) Model
-model = mlogit(School ~  Dist, data=mldf, reflevel = 'SG')
-summary(model)
+model = mlogit(School ~ Dist + Enrol | 0, data=mldf, reflevel='SG')
+coef(model)
 mean(fitted(model))
 
 # Nested Model
-nest_model = mlogit(School ~  Dist, data=mldf, reflevel = 'SG', nests = list(uni = unis, col = cols))
-summary(nest_model)
+nest_model = mlogit(School ~ Dist + Enrol | 0, data=mldf, reflevel='SG', nests=list(uni=unis, col=cols))
+coef(nest_model)
 mean(fitted(nest_model))
 
 # Nested Model - Unique Elasticities
-nest_model_unique = mlogit(School ~  Dist, data=mldf, reflevel = 'SG', nests = list(uni = unis, col = cols), un.nest.el=TRUE)
-summary(nest_model_unique)
-mean(fitted(nest_model_unique))
+one_iv = update(nest_model, un.nest.el=TRUE)
+coef(one_iv)
+mean(fitted(one_iv))
 
 # Nested Model - Unscaled
-nest_model_unscaled = mlogit(School ~  Dist, data=mldf, reflevel = 'SG', nests = list(uni = unis, col = cols), unscaled=TRUE)
-summary(nest_model_unscaled)
-mean(fitted(nest_model_unscaled))
+unscaled = update(nest_model, unscaled=TRUE)
+coef(unscaled)
+mean(fitted(unscaled))
+
+lrtest(model, one_iv)
+lrtest(one_iv, nest_model)
+lrtest(unscaled, nest_model)
+
+iv = coef(nest_model)['iv:uni'] # 1 - iv is correlation in unobserved factors within nest
+var = vcov(nest_model)['iv:uni', 'iv:uni']
+(iv - 1) / sqrt(var) # t-test for that iv = 1
